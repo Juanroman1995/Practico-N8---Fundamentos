28d6b321aa859fb9d0ac1b1a87f347aa3327330b
Juanroman1995 <109772447+Juanroman1995@users.noreply.github.com>
2022-08-23 14:11:18 -0300

    Update Ejercicios.js

--- Ejercicios.js
+++ Ejercicios.js
@@ -1,20 +1,43 @@
-function invocarCallback(cb) {
+//function invocarCallback(cb) {
     // Invoca al callback `cb`
     // Aqui tu codigo
+//}
+function saludo(usuario){
+    return 'Hola!' + usuario
 }
+function saludar(usuario,callback){
+    return callback(usuario)
+}
+console.log(saludar('Juan', saludo))
 
-function sumarArray(numeros, cb) {
+
+//function sumarArray(numeros, cb) {
     // Suma todos los números enteros (int/integers) de un array ("numeros")
     // Pasa el resultado a `cb`
     // No es necesario devolver nada
     // Aqui tu codigo
-}  
+//}  
 
-function forEach(array, cb) {
+function sumarArray(array){
+    return array
+}
+function cb(array,cb){
+    return cb(array)
+}
+//console.log (cb([1,2,3,4],sumarArray)) resultado esperado //[1,2,3,4]
+
+
+//function forEach(array, cb) {
     // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
     // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
     // Aqui tu codigo
-}  
+//}  
+var array = [1,2,3,4,5,6,7]
+function Array(element, index, array) {
+    console.log(index + " ---> " + element);
+}
+array.forEach(Array);
+
 
 function map(array, cb) {
     // Crea un nuevo array
